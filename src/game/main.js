9e7e380a173df8593a8247b44559b6d10b7f0568
legendarie <tle1@macalester.edu>
2018-10-17 20:42:10 -0500

    santa - second trial

--- src/game/main.js
+++ src/game/main.js
@@ -7,31 +7,49 @@
  */
 
 var state = {
-    init: function() {
-        // Delete this init block or replace with your own logic.
 
-        // Create simple text display for current Phaser version
-        var text = "Phaser Version "+Phaser.VERSION + " works!";
-        var style = { font: "24px Arial", fill: "#fff", align: "center" };
-        var t = game.add.text(this.world.centerX, this.world.centerY, text, style);
-        t.anchor.setTo(0.5, 0.5);
-
-    },
     preload: function() {
         // STate preload logic goes here
+        this.game.load.image('bg', 'assets/background.jpg');
+        this.game.load.image('santa', 'assets/Picture1.png');
+        this.game.load.image('candy', 'assets/candy.png');
+
     },
     create: function(){
       // State create logic goes here
+        this.background = this.game.add.sprite(0,0,'bg');this.background.scale.setTo(0.5,0.5)
+        this.candy = this.game.add.sprite(this.game.world.centerX,this.game.world.height,'candy');this.candy.anchor.setTo(0.5,1)
+        this.santa = this.game.add.sprite(0,this.game.world.centerY,'santa');this.santa.anchor.setTo(0.5,0.5)
+
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.physics.enable(this.candy);
+        this.candy.body.collideWorldBounds = true;
+        // this.candy.body.gravity.x = 10;
+        this.candy.body.velocity.x = 200;
+        this.candy.body.velocity.y = 500;
+        this.candy.body.gravity.y = 100 + Math.random() * 100;
+        this.candy.body.bounce.setTo(1, 1);
+
+        this.game.physics.enable(this.santa);
+        this.santa.body.collideWorldBounds = true;
+        this.santa.body.velocity.setTo(200,-5);
+        this.santa.body.bounce.setTo(1,1);
     },
     update: function() {
         // State Update Logic goes here.
+        // this.candy.x += 5;
+        // this.candy.y -= 2;
+        this.game.physics.arcade.collide(this.candy, this.santa);
     }
+
+
 };
 
 var game = new Phaser.Game(
-    800,
-    480,
+    1200,
+    720,
     Phaser.AUTO,
     'game',
     state
 );
+
