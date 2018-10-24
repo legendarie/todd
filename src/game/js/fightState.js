dcbe6fee0c36c751f611d1bbdf2c5dfecee6d850
snederveld <snederve@macalester.edu>
2018-10-24 17:28:37 -0500

    Made sprites global variables

--- src/game/js/fightState.js
+++ src/game/js/fightState.js
@@ -1,3 +1,8 @@
+var candy;
+var santa;
+var background;
+var slingshot;
+
 var fightState = {
 
     preload: function () {
@@ -12,29 +17,28 @@ var fightState = {
 
     create: function() {
         // adds the background to the game world
-        this.background = game.add.image(0, 0, 'bg');
-        this.background.scale.setTo(0.5, 0.5);
+        background = game.add.image(0, 0, 'bg');
+        background.scale.setTo(0.5, 0.5);
 
         // randomly chooses between the two candy sprites and adds it to the world
-        this.candy = null;
         var num = Math.random();
         if (num < 0.5) {
-            this.candy = game.add.sprite(game.world.centerX, game.world.height, 'dotCandy');
-            this.candy.scale.setTo(0.1,0.1);
-            this.candy.anchor.setTo(0.5, 1);
+            candy = game.add.sprite(game.world.centerX, game.world.height, 'dotCandy');
+            candy.scale.setTo(0.1,0.1);
+            candy.anchor.setTo(0.5, 1);
         } else {
-            this.candy = game.add.sprite(game.world.centerX, game.world.height, 'stripeCandy');
-            this.candy.scale.setTo(0.1,0.1);
-            this.candy.anchor.setTo(0.5, 1);
+            candy = game.add.sprite(game.world.centerX, game.world.height, 'stripeCandy');
+            candy.scale.setTo(0.1,0.1);
+            candy.anchor.setTo(0.5, 1);
         }
 
-        this.slingshot = game.add.sprite(game.world.centerX, game.world.height, 'slingshot');
-        this.slingshot.scale.setTo(0.07, 0.07);
-        this.slingshot.anchor.setTo(0.5, 0.5);
+        slingshot = game.add.sprite(game.world.centerX, game.world.height, 'slingshot');
+        slingshot.scale.setTo(0.07, 0.07);
+        slingshot.anchor.setTo(0.5, 0.5);
 
         // add the santa sprite to the middle of the world
-        this.santa = game.add.sprite(game.world.centerX, game.world.centerY, 'santa');
-        this.santa.anchor.setTo(0.5, 0.5);
+        santa = game.add.sprite(game.world.centerX, game.world.centerY, 'santa');
+        santa.anchor.setTo(0.5, 0.5);
 
 
         // enables the arcade-style physics pre-loaded into Phaser
@@ -42,25 +46,26 @@ var fightState = {
 
         // enables game physics to apply to the candy sprite, then sets its x and y velocity, the gravity
         // that will be applied to the sprite when it falls, and the x and y trajectory upon a bounce
-        game.physics.enable(this.candy);
-        this.candy.body.collideWorldBounds = true;
-        this.candy.body.gravity.x = 10;
-        this.candy.body.velocity.x = 200;
-        this.candy.body.velocity.y = 500;
+        game.physics.enable(candy);
+        candy.body.collideWorldBounds = true;
+        candy.body.gravity.x = 10;
+        candy.body.velocity.x = 200;
+        candy.body.velocity.y = 500;
         //this.candy.body.gravity.y = 100 + Math.random() * 100;
-        this.candy.body.bounce.setTo(1, 1);
+        candy.body.bounce.setTo(1, 1);
 
         // enables the santa sprite to be interacted with, but does not allow it to move through the world
-        game.physics.enable(this.santa);
-        this.santa.body.immovable = true;
+        game.physics.enable(santa);
+        santa.body.immovable = true;
 
         //fightState.update(candy, santa);
 
     },
 
+
     update: function() {
         // as the game is running, the candy and santa are allowed to interact (to bounce off one another)
-        game.physics.arcade.collide(this.candy, this.santa);
+        game.physics.arcade.collide(candy, santa);
     }
 
 };
