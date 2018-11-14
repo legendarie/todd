c2b9feb6693df5513d2676c7fff6edad5fd4caab
Linnea <lprehn@macalester.edu>
2018-11-14 10:55:44 -0600

    Hopefully help with merge heh

--- src/game/js/main.js
+++ src/game/js/main.js
@@ -10,4 +10,4 @@ game.state.add('caveStartState', caveStartState);
 game.state.add('fightState', fightState);
 game.state.add('findingSanta', findingSanta);
 
-game.state.start('findingSanta');
+game.state.start('ddorState');
