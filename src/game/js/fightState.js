b45c70c5ff0bb9034df7f53ce3370d71672b647c
legendarie <tle1@macalester.edu>
2018-11-15 11:43:53 -0600

    Update fightState

--- src/game/js/fightState.js
+++ src/game/js/fightState.js
@@ -2,6 +2,9 @@ let count = 0;
 var originalX;
 var originalY;
 var candy;
+var santa;
+var numCollision;
+var slingShot;
 
 var fightState = {
 
@@ -22,43 +25,40 @@ var fightState = {
 
         // enables the arcade-style physics pre-loaded into Phaser
         game.physics.startSystem(Phaser.Physics.ARCADE);
-
         this.createSlingshot();
         this.createCandy();
         this.createSanta();
-        // this.launch();
+        numCollision = 0;
 
-        // if(count==3){
-        //     text = game.add.text(0,0,'you win!');
-        //     text.anchor.setTo(0.5);
-        //     text.position.setTo(game.world.width/2,game.world.height/2);
-        // }
+    },
 
-        // this.play();
-
-
-
-
+    reset: function() {
+        santa.destroy();
+        candy.destroy();
+        slingShot.destroy();
+        this.createSlingshot();
+        this.createCandy();
+        this.createSanta();
+        numCollision = 0;
     },
 
     createCandy: function() {
         // randomly chooses between the two candy sprites and adds it to the world
-        // var num = Math.random();
-        // if (num < 0.5) {
-        candy = game.add.sprite(game.world.centerX, 550, 'dotCandy');
-        candy.scale.setTo(0.1);
-        candy.anchor.setTo(0.5, 1);
-        // } else {
-        //     candy = game.add.sprite(game.world.centerX, 550, 'stripeCandy');
-        //     candy.scale.setTo(0.1);
-        //     candy.anchor.setTo(0.5, 1);
-        // }
+        var num = Math.random();
+        if (num < 0.5) {
+            candy = game.add.sprite(game.world.centerX, 550, 'dotCandy');
+            candy.scale.setTo(0.1);
+            candy.anchor.setTo(0.5, 1);
+        } else {
+            candy = game.add.sprite(game.world.centerX, 550, 'stripeCandy');
+            candy.scale.setTo(0.1);
+            candy.anchor.setTo(0.5, 1);
+        }
         originalX = candy.x;
         originalY = candy.y;
         game.physics.enable(candy);
         candy.inputEnabled = true;
         candy.input.enableDrag(true);
-        // candy.events.onInputDown.add(candy.input.enableDrag(true),this);
 
         this.play();
     },
@@ -76,16 +76,20 @@ var fightState = {
         santa.position.setTo(Math.floor(Math.random() * (game.world.width-santa.width)),Math.floor(Math.random() * (game.world.height-santa.height)));
         santa.scale.setTo(0.5);
         game.physics.enable(santa);
-        santa.body.immovable = true;
+        santa.body.collideWorldBounds = true;
+        santa.body.bounce.setTo(0.6,0.6);
         santa.inputEnabled = true;
         santa.events.onInputDown.add(santa.input.enableDrag(true), this);
 
     },
 
+    // createLine: function() {
+    //
+    // },
+
     play: function() {
         count++;
-        this.launch();
-        // candy.events.onInputUp.add(this.launch,this);
+        candy.events.onInputUp.add(this.launch,this);
 
 
     },
@@ -95,20 +99,28 @@ var fightState = {
         // enables game physics to apply to the candy sprite, then sets its x and y velocity, the gravity
         // that will be applied to the sprite when it falls, and the x and y trajectory upon a bounce
         candy.body.collideWorldBounds = true;
-        candy.body.gravity.y = 100;
-        // distanceX = Math.abs(candy.x - originalX);
-        // distanceY = Math.abs(candy.y - originalY);
-        candy.body.velocity.x = 100;
-        candy.body.velocity.y = -100;
-        //this.candy.body.gravity.y = 100 + Math.random() * 100;
-        candy.body.bounce.setTo(1, 1);
 
+        distanceX = (candy.x - originalX);
+        distanceY = (candy.y - originalY);
+
+        candy.body.gravity.y = 100;
+        candy.body.velocity.x = -5*distanceX;
+        candy.body.velocity.y = -5*distanceY;
+        candy.body.bounce.setTo(.8, .8);
+
+    },
+
+    incrementCollision: function() {
+        numCollision++;
     },
 
 
     update: function() {
         // as the game is running, the candy and santa are allowed to interact (to bounce off one another)
-        game.physics.arcade.collide(candy, santa);
+        game.physics.arcade.collide(candy, santa, this.reset);
+        // if(numCollision == 3) {
+        //     this.create();
+        // }
     }
 
 };
